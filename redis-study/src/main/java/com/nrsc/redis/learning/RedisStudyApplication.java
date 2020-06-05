93b60f64c7cd624cf28db51678618c4c2cd081bc
sunchuan <272420199@qq.com>
2020-06-06 07:59:14 +0800

    change module name

--- redis-study/src/main/java/com/nrsc/redis/learning/RedisStudyApplication.java
+++ redis-study/src/main/java/com/nrsc/redis/learning/RedisStudyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearningApplication {
+public class RedisStudyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LearningApplication.class, args);
+        SpringApplication.run(RedisStudyApplication.class, args);
     }
 
 }
